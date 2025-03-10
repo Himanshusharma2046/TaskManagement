901f4b1cc96a66038592a71932c16e2756d27dbc
Himanshusharma2046 <himanshusharma4041@gmail.com>
2025-03-10 23:55:26 +0530

    Setting mongodb

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,16 +1,11 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('Connected to MongoDB');
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('mongodb connected successfully');
     } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
-        process.exit(1);
+        console.log(error);
     }
-};
-
-module.exports = connectDB;
+}
+export default connectDB;
