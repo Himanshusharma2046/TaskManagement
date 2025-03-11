507424ec68267ca8a47fcb593631bf684a380fb9
Himanshusharma2046 <106347612+Himanshusharma2046@users.noreply.github.com>
2025-03-11 12:40:27 +0530

    Update taskService.js

--- frontend/src/services/taskService.js
+++ frontend/src/services/taskService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/tasks';
+const API_URL = 'https://taskmanagement-backend-0rn2.onrender.com';
 
 export const getTasks = async () => axios.get(API_URL);
 export const addTask = async (title) => axios.post(API_URL, { title });
